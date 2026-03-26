611368116addbfeb494d06012b0d4a482716a87d
MickeAsonn <tabmicke@gmail.com>
2026-03-26 09:19:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
 
-let entries = [];
-function save(){
-  const form = new FormData(document.getElementById('form'));
-  const obj = {};
-  form.forEach((v,k)=> obj[k]=v);
-  document.querySelectorAll('input[type=checkbox]').forEach(cb=> obj[cb.name]=cb.checked?'Ja':'');
-  entries.push(obj);
-  console.log(entries);
-  alert('Sparad!');
-}
+function save(){ alert('Formulär sparat (demo)!'); }
